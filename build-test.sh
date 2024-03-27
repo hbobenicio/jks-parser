7a64f36a3a733898249bf611ed11d80919fd065a
hbobenicio <hbobenicio@gmail.com>
2024-03-27 20:23:32 -0300

    versioning build script

---
+++ build-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu -o pipefail
+
+CC="clang-15"
+PKG_CONFIG_DEPS="openssl"
+
+CFLAGS+=" -std=c18 -Wall -Wextra -Wpedantic -Wno-vla -Wswitch-enum"
+CFLAGS+=" -O0 -g -fsanitize=address,undefined -fno-omit-frame-pointer"
+# CFLAGS+=" -O3 -march=native -flto=thin -D_FORTIFY_SOURCE=3"
+CFLAGS+=" `pkg-config --cflags $PKG_CONFIG_DEPS`"
+
+LIBS+=" `pkg-config --libs $PKG_CONFIG_DEPS`"
+
+set -x
+"$CC" $CFLAGS -o tests tests.c $LIBS
+{ set +x; } 2> /dev/null
